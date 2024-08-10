c05b124fcdb452c92930ece61b67029c376da5f6
nahid <eng.yanahid@gmail.com>
2024-08-10 23:30:32 +0600

    Create capturing webpage screenshot project

---
+++ WebpageScreenshot/src/main/java/com/springboot/WebpageScreenshot/ScreenshotApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.WebpageScreenshot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenshotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenshotApplication.class, args);
+	}
+
+}
---
+++ WebpageScreenshot/src/main/java/com/springboot/WebpageScreenshot/ScreenshotService.java
@@ -0,0 +1,7 @@
+package com.springboot.WebpageScreenshot;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ScreenshotService {
+}
