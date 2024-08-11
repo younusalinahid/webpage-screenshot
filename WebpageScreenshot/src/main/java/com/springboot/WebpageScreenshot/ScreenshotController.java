bb2ae5990039475d9e5aac7b96f3bab8100d26a3
nahid <eng.yanahid@gmail.com>
2024-08-11 21:31:47 +0600

    ChromeDriver setup is and configure properties

--- WebpageScreenshot/src/main/java/com/springboot/WebpageScreenshot/ScreenshotController.java
+++ WebpageScreenshot/src/main/java/com/springboot/WebpageScreenshot/ScreenshotController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URISyntaxException;
+
 @RestController
 public class ScreenshotController {
 
@@ -12,7 +14,7 @@ public class ScreenshotController {
     private ScreenshotService screenshotService;
 
     @GetMapping("/screenshot")
-    public String takeScreenshot(@RequestParam String url, @RequestParam String outputPath) {
+    public String takeScreenshot(@RequestParam String url, @RequestParam String outputPath) throws URISyntaxException {
         screenshotService.takeScreenshot(url, outputPath);
         return "Screenshot saved to " + outputPath;
     }
--- WebpageScreenshot/src/main/java/com/springboot/WebpageScreenshot/ScreenshotService.java
+++ WebpageScreenshot/src/main/java/com/springboot/WebpageScreenshot/ScreenshotService.java
@@ -6,11 +6,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 import org.apache.commons.io.FileUtils;
+
 import java.io.File;
 import java.io.IOException;
-
 
 @Service
 public class ScreenshotService {
@@ -19,27 +20,33 @@ public class ScreenshotService {
     private String chromeDriverPath;
 
     public void takeScreenshot(String url, String outputPath) {
-
-        System.setProperty("webdriver.chrome.driver", chromeDriverPath);
-
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--handless");
-        options.addArguments("--window-size=1920x1080");
-
-        WebDriver driver = new ChromeDriver(options);
         try {
-            driver.get(url);
-            File file = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            ClassPathResource classPathResource = new ClassPathResource(chromeDriverPath);
+            File chromeDriverFile = classPathResource.getFile();
+            System.setProperty("webdriver.chrome.driver", chromeDriverFile.getAbsolutePath());
 
-            File outputFile = new File(outputPath);
-            FileUtils.copyFile(file, outputFile);
-            System.out.println("Screenshot save to: " + outputPath);
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");
+            options.addArguments("--window-size=1920x1080");
+
+            WebDriver driver = new ChromeDriver(options);
+            try {
+                driver.get(url);
+                File file = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+                File outputFile = new File(outputPath);
+                FileUtils.copyFile(file, outputFile);
+                System.out.println("Screenshot saved to: " + outputPath);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                driver.quit();
+            }
 
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            driver.quit();
         }
-
     }
 }
+
