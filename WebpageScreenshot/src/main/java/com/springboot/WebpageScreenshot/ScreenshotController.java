ba0af05d859ce0d2c78b6b20e8617c344e0c9cc8
nahid <eng.yanahid@gmail.com>
2024-08-11 16:36:07 +0600

    Implement screenshot functionality with Selenium

--- WebpageScreenshot/src/main/java/com/springboot/WebpageScreenshot/ScreenshotController.java
+++ WebpageScreenshot/src/main/java/com/springboot/WebpageScreenshot/ScreenshotController.java
@@ -1,7 +1,20 @@
 package com.springboot.WebpageScreenshot;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ScreenshotController {
+
+    @Autowired
+    private ScreenshotService screenshotService;
+
+    @GetMapping("/screenshot")
+    public String takeScreenshot(@RequestParam String url, @RequestParam String outputPath) {
+        screenshotService.takeScreenshot(url, outputPath);
+        return "Screenshot saved to " + outputPath;
+    }
+
 }
--- WebpageScreenshot/src/main/java/com/springboot/WebpageScreenshot/ScreenshotService.java
+++ WebpageScreenshot/src/main/java/com/springboot/WebpageScreenshot/ScreenshotService.java
@@ -1,7 +1,45 @@
 package com.springboot.WebpageScreenshot;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+import java.io.IOException;
+
 
 @Service
 public class ScreenshotService {
+
+    @Value("${chrome.driver.path}")
+    private String chromeDriverPath;
+
+    public void takeScreenshot(String url, String outputPath) {
+
+        System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--handless");
+        options.addArguments("--window-size=1920x1080");
+
+        WebDriver driver = new ChromeDriver(options);
+        try {
+            driver.get(url);
+            File file = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+
+            File outputFile = new File(outputPath);
+            FileUtils.copyFile(file, outputFile);
+            System.out.println("Screenshot save to: " + outputPath);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            driver.quit();
+        }
+
+    }
 }
